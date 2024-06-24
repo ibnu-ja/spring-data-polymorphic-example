d616511d9615503e1a07bf4b4e072610456c281c
guest.02 <admin-das@mcf.co.id>
2024-06-24 15:07:45 +0700

    feat: create test

--- src/test/kotlin/io/ibnuja/springdatapolymorphicexample/SpringDataPolymorphicExampleApplicationTests.kt
+++ src/test/kotlin/io/ibnuja/springdatapolymorphicexample/SpringDataPolymorphicExampleApplicationTests.kt
@@ -1,13 +1,22 @@
 package io.ibnuja.springdatapolymorphicexample
 
+import io.ibnuja.polymorphicexample.media.Media
+import io.ibnuja.polymorphicexample.media.MediaRepo
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertEquals
 
 @SpringBootTest
-class SpringDataPolymorphicExampleApplicationTests {
+class SpringDataPolymorphicExampleApplicationTests(
+	private val mediaRepo: MediaRepo
+) {
 
 	@Test
-	fun contextLoads() {
+	@Order(1)
+	fun createMedia() {
+		val media = mediaRepo.save(Media(name = "file1"))
+		assertEquals(media.id, 1L)
+		assertEquals(media.name, "file1")
 	}
-
 }
